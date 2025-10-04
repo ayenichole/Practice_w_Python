94e22b9d6c5744bd9a1d93280d038a20874bafea
Nichole Denga <161051399+ayenichole@users.noreply.github.com>
2025-10-04 03:01:47 +0200

    Add files via upload

---
+++ tuplesinpy.py
@@ -0,0 +1,17 @@
+#experimenting with tuples
+tuple_1 = (1, 2, 4, 8)
+tuple_2 = 1., .5, .25, .125
+
+print(tuple_1)
+print(tuple_2)
+
+# another example witha  tuple
+my_tuple = (1, 10, 100, 1000)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print(my_tuple[1:])
+print(my_tuple[:-2])
+
+for elem in my_tuple:
+    print(elem)
