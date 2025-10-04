642d981165a1d5be54b41c7bd552cd82be24e4d1
Nichole Denga <161051399+ayenichole@users.noreply.github.com>
2025-10-05 01:31:01 +0200

    Add files via upload

---
+++ student_averages.py
@@ -0,0 +1,27 @@
+#calculating the average score of students
+#the program allows the user to enter name and score of te students which is stored in a dictionary and a tuple
+#the program also shows the intersection of tuples and dictionaries
+
+school_class = {}
+
+while True:
+    name = input("Enter the student's name: ")
+    if name == '':
+        break
+    
+    score = int(input("Enter the student's score (0-10): "))
+    if score not in range(0, 11):
+        break
+
+    if name in school_class:
+        school_class[name] += (score,)
+    else:
+        school_class[name] = (score,)
+        
+for name in sorted(school_class.keys()):
+    adding = 0
+    counter = 0
+    for score in school_class[name]:
+        adding += score
+        counter += 1
+    print(name, ":", adding / counter)
